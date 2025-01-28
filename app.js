e9bfc28a1dd397b36c6e83dd01b6b84cf2a635dc
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-01-27 23:01:19 -0600

    Se agrega funcion de asignarTextoElemento y agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+nombres = [];
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function agregarAmigo(lista) {
+    let nombre = document.getElementById('amigo').value
+    lista.push(nombre);
+    return lista;
+}
+
+function actualizarAmigos(lista) {
+    const lista = document.getElementById('listaAmigos');
+}
+
