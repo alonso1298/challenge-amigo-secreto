776d01261f1a075ad8da33fd4afed81dc1f23404
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-01-28 00:03:47 -0600

    Se termina la funcion de a<ctualizarAmigo

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 
-nombres = [];
+let nombres = [];
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
 }
 
-function agregarAmigo(lista) {
+function agregarAmigo() {
     let nombre = document.getElementById('amigo').value
     if(nombre == '') {
         alert('Por favor, inserte un nombre')
     }else {
-        lista.push(nombre);
+        nombres.push(nombre);
+        actualizarAmigos()
         document.getElementById('amigo').value = ''
     }
-    return lista;
 }
-
-
-console.log(agregarAmigo(nombres))
 
 function actualizarAmigos() {
     const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '' // Se limpia la lista antes de actualizar
+    for(let i = 0; i < nombres.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = nombres[i];
+        lista.appendChild(li)
+    }
 }
 
